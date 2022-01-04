9198e0f3ab3dabe90c8fd9fcbb9119fa9bd8f8f0
jiaqi34 <jiaqi34@staff.sina.com.cn>
2022-01-05 01:28:28 +0800

    c

--- src/main/java/com/jiaqi/leetgod/Solution4.java
+++ src/main/java/com/jiaqi/leetgod/Solution4.java
@@ -2,6 +2,12 @@ package com.jiaqi.leetgod;
 
 import java.util.*;
 
+/**
+ * 1)输入是List<Person>有id,age,height三个字段
+ * 2)输出也是List<Person>,其中同样年龄的只输出身高最高的，年龄身高都相同的，只输出id最大的。
+ *	https://codeleading.com/article/63975938164/
+ * 解法：优先级队列
+ */
 public class Solution4 {
     public static void main(String[] args) {
         List<Person> list = new ArrayList<>();
@@ -24,6 +30,7 @@ public class Solution4 {
             PriorityQueue<Person> temp = map.get(person.getAge());
             if (temp == null) {
                 temp = new PriorityQueue<>((o1, o2) -> {
+                    //o1代表当前对象，return 1 代表当前对象在队列中往后排
                     if (o1.getHeight() - o2.getHeight() < 0) {
                         return 1;
                     } else if (o1.getHeight() - o2.getHeight() == 0) {
@@ -37,6 +44,7 @@ public class Solution4 {
             map.put(person.getAge(), temp);
         }
         List<Person> returnList = new ArrayList<>();
+        //poll()获取并移除此队列的头，如果此队列为空，则返回 null。
         map.entrySet().forEach(x -> returnList.add(x.getValue().poll()));
 
         return returnList;
